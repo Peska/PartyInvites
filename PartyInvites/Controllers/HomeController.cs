f646c253815eeba59b28e3a5829d5d0203e7229b
Peska <peska_jr@o2.pl>
2015-08-23 16:59:54 +0200

    My First View

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+    }
+}
