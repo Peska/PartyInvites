922e5a0b16535dd0dd3192579da8ee988c792c41
Peska <peska_jr@o2.pl>
2015-08-24 21:12:36 +0200

    Created RSVP Form

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -3,14 +3,19 @@ using System.Web.Mvc;
 
 namespace PartyInvites.Controllers
 {
-    public class HomeController : Controller
-    {
-        // GET: Home
-        public ViewResult Index()
-        {
-            int hour = DateTime.Now.Hour;
-            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
-            return View();
-        }
+	public class HomeController : Controller
+	{
+		// GET: Home
+		public ViewResult Index()
+		{
+			int hour = DateTime.Now.Hour;
+			ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+			return View();
+		}
+
+		public ViewResult RsvpForm()
+		{
+			return View();
+		}
     }
 }
